adc737919c9367cbfbd1eed5f6513bda63dbaa27
thuy <caiphuongthuy1110@gmail.com>
2025-10-27 13:31:42 +0700

    hiển thị giao diện trạng thái đăng nhậpnhập

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false   // tránh console log ra 2 lần 
 };
 
 export default nextConfig;
---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,28 @@
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react"
+
+export const useAuth = () => {
+  const [ isLogin, setIsLogin ] = useState(false);
+  const [ infoUser, setInfoUser ] = useState<any>(null);
+  const pathName = usePathname();
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/check`, {
+      credentials: "include"  // gửi kèm cookie
+    })
+      .then(res => res.json())
+      .then(data => {
+        if(data.code == "error"){
+          setIsLogin(false);
+        }
+
+        if(data.code == "success"){
+          setIsLogin(true);
+          setInfoUser(data.infoUser);
+        }
+      })
+  }, [pathName]); // call api gửi cookie để check cookie có hợp lệ ko
+  // mỗi lần chuyển trang thì useEffect gọi lại api
+
+  return { isLogin, infoUser };
+}
